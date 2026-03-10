b7f82da83a596afe9d058e7094a31cabdb19289a
twentyone <dmytro.demianenko21@gmail.com>
2026-03-10 22:00:13 +0100

    Initial commit with bff module

---
+++ apps/bff/src/main/java/org/shelfio/bff/BffApplication.java
@@ -0,0 +1,13 @@
+package org.shelfio.bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(BffApplication.class, args);
+    }
+
+}
