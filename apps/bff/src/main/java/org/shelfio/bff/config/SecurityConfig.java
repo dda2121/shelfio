4be9bd738133188c4a056084d6cf08090404a24b
twentyone <dmytro.demianenko21@gmail.com>
2026-03-26 21:27:39 +0100

    BFF Skeleton setup.
    Provided basic security configuration utilizing Spring Session and Redis to store these sessions. Configured Oauth2 authorization code flow.

---
+++ apps/bff/src/main/java/org/shelfio/bff/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package org.shelfio.bff.config;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@Configuration
+@EnableRedisHttpSession
+public class SecurityConfig {
+
+    /**
+     * TODO: CORS and CSRF implementation will be provided later.
+     */
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) {
+        http.csrf(Customizer.withDefaults())
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/oauth2/**", "/login/oauth2/**").permitAll()
+                        .anyRequest().authenticated())
+                .oauth2Login(Customizer.withDefaults())
+                .logout(logout -> logout
+                        .deleteCookies("JSESSIONID", "SESSION") // TODO: Remove JSESSIONID after bug is fixed
+                        .logoutSuccessHandler((_, res, _) ->
+                                res.setStatus(HttpServletResponse.SC_OK)))
+                .sessionManagement(session -> session
+                        .sessionFixation(SessionManagementConfigurer.SessionFixationConfigurer::migrateSession));
+        return http.build();
+    }
+}
