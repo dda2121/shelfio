73925d24e52a5405e131dae92dc3ad8fa6150e80
twentyone <dmytro.demianenko21@gmail.com>
2026-03-29 20:58:09 +0200

    Refactor security configuration for reactive support and update dependencies

--- apps/gateway/src/main/java/org/shelfio/gateway/config/SecurityConfig.java
+++ apps/gateway/src/main/java/org/shelfio/gateway/config/SecurityConfig.java
@@ -1,34 +1,30 @@
 package org.shelfio.gateway.config;
 
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.session.data.redis.config.annotation.web.server.EnableRedisWebSession;
 
 @Configuration
-@EnableRedisHttpSession
+@EnableRedisWebSession
 public class SecurityConfig {
-
     /**
-     * TODO: CORS and CSRF implementation will be provided later.
+     * TODO
+     * [] CSRF implementation
+     * [] CORS implementation
+     * [] Logout implementation (success handler + delete cookies)
+     * [] Session management implementation (session fixation protection).
+     *    Check how sessions work in reactive apps.
      */
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) {
+    public SecurityWebFilterChain webFilterChain(ServerHttpSecurity http) {
         http.csrf(Customizer.withDefaults())
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/oauth2/**", "/login/oauth2/**").permitAll()
-                        .anyRequest().authenticated())
-                .oauth2Login(Customizer.withDefaults())
-                .logout(logout -> logout
-                        .deleteCookies("JSESSIONID", "SESSION") // TODO: Remove JSESSIONID after bug is fixed
-                        .logoutSuccessHandler((_, res, _) ->
-                                res.setStatus(HttpServletResponse.SC_OK)))
-                .sessionManagement(session -> session
-                        .sessionFixation(SessionManagementConfigurer.SessionFixationConfigurer::migrateSession));
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/oauth2/**", "/login/oauth2/**").permitAll()
+                        .anyExchange().authenticated())
+                .oauth2Login(Customizer.withDefaults());
         return http.build();
     }
 }
